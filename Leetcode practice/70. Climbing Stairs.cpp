87e82404d21f1623964c9792b9b98390abb67502
M Tahmid Hossain <tahmidolee@Ms-MacBook-Pro.mshome.net>
2018-06-30 11:48:47 +0800

    70. Climbing Stairs.cpp added

---
+++ Leetcode practice/70. Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 1) return 1;
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
+
+int main(){
+    Solution sol;
+    cout << sol.climbStairs(4)<<endl;
+}
